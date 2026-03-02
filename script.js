3ca982d7c3625aad0f59baad74edc3f58138d8d2
ZeeBedie <131893782+ZeeBedie@users.noreply.github.com>
2026-03-02 16:10:59 +0000

    completed the JS

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-//desktop test
+const newColourBtnElement = document.getElementById("new-colour-button");
+const currentColourElement = document.getElementById("current-colour");
+
+const hexValues = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+];
+
+function getRandomHexValue() {
+  const randomIndexPosition = Math.floor(Math.random() * hexValues.length);
+
+  return hexValues[randomIndexPosition];
+}
+
+function getRandomHexString(stringLength) {
+  let hexString = "";
+
+  for (let i = 0; i < stringLength; i++) {
+    hexString += getRandomHexValue();
+  }
+
+  return hexString;
+}
+
+newColourBtnElement.addEventListener("click", () => {
+  const randomHexString = "#" + getRandomHexString(6);
+
+  document.body.style.backgroundColor = randomHexString;
+  currentColourElement.textContent = randomHexString;
+});
